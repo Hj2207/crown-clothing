0e677c791097b425dd30cba6e1e70320ceb0be70
Harshita <harshita@github.com>
2021-02-09 13:36:34 +0530

    added cart action and reducers

--- src/Components/collection-item/collection-item.component.jsx
+++ src/Components/collection-item/collection-item.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.action';
+import CustomButton from '../Custom-button/custom-button.component'
 
-const CollectionItem = ({ name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
     <div className='collection-item'>
       <div
         className='image'
@@ -13,6 +18,13 @@ const CollectionItem = ({ name, price, imageUrl }) => (
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton inverted onClick={()=>addItem(item)}>Add To Cart</CustomButton>
     </div>
-  );
-export default CollectionItem;
+  )};
+
+
+  const mapDispatchToProps = dispatch => ({
+    addItem: (item) => dispatch(addItem(item))
+  })
+
+export default connect(null,mapDispatchToProps)(CollectionItem);
--- src/Components/collection-preview/collection-preview.component.jsx
+++ src/Components/collection-preview/collection-preview.component.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import CollectionItem from '../collection-item/collection-item.component'
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, id, items}) => (
+const CollectionPreview = ({title, items}) => (
 
     <div className='collection-preview'>
         <div className= 'title'>{title.toUpperCase()}</div>
         <div className='preview'>
         {
-           items.filter((id,idx)=> idx< 4).map(({id, ...otherCollectionData}) => (
-               <CollectionItem key={id} {...otherCollectionData}/>
+           items.filter((item,idx)=> idx< 4).map(item => (
+               <CollectionItem key={item.id} item={item}/>
            )) 
         }
         </div>
